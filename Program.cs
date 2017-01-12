a20df0be273c8cd267ca801558a8832477d6dae0
Donovan An <donovantaean@yahoo.com>
2017-01-11 17:02:07 -0800

    created human class with default attr

--- Program.cs
+++ Program.cs
@@ -2,11 +2,31 @@
 
 namespace ConsoleApplication
 {
+    public class Human{
+        public string name;
+        public int strength;
+        public int intelligence;
+        public int dexterity;
+        public int health;
+
+
+        public Human(string nameVal){
+            name = nameVal;
+            strength = 3;
+            intelligence = 3;
+            dexterity = 3;
+            health = 100;
+        }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Human donovan = new Human("Donovan");
+            Console.WriteLine(donovan.name);
+            Console.WriteLine(donovan.strength);
+            Console.WriteLine(donovan.health);
         }
     }
 }
